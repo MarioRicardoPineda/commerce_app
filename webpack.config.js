1dd1d1e0a9e007c945e185479dea233efecf8514
Ricardo Pineda <ricar1997@outlook.com>
2019-07-06 08:56:05 -0600

    [ADDED] Web de Commerce_app

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+
+// const polyfill = require('')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+// let HtmlPlugin = new HtmlWebpackPlugin({
+//     template : './src/index.html'          
+// })
+
+// let paginas = ['prueba']
+
+// let multiplesFiles =  paginas.map(nameFile => {
+//     return new HtmlWebpackPlugin({
+//         filename : nameFile + '.html',
+//         template : __dirname + `./src/${nameFile}.html`
+//     })
+// })
+
+module.exports = {
+
+    resolve : {
+        modules : [
+            'src',
+            'node_modules'
+        ]
+    },
+
+    entry: {
+        app: ['./src/app.js', '@babel/polyfill']
+    },
+
+    output : {
+        path : path.resolve(__dirname, '/public'),
+        filename: 'js/main.js'
+    },
+
+    devServer : {
+        port : 5000
+    },
+
+    module : {
+        rules : [
+            {
+                test : /\.(sa|sc|c|)ss$/,
+                use : [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test : /\.html$/,
+                use : 'html-loader'
+            },
+            {
+                test : /\.(jpe?g|svg|icon|png|gif)(\?[a-z0-9]+)?$/,
+                use : 'url-loader'
+            },
+            {
+                test : /\.js$/,
+                exclude : /node_modules/,
+                use : 'babel-loader'
+            }
+        ]
+    },
+
+    plugins : [
+        new MiniCssExtractPlugin({
+            filename : 'css/[name]-style.css'
+        }),
+        new HtmlWebpackPlugin({
+            template : './src/index.html'          
+        })
+    ] //.concat(multiplesFiles)
+
+}
