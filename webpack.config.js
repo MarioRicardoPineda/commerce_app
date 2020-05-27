aa3b45056429feba3c15fac69a6387b641e6bebb
Ricardo Pineda <MarioRicardoPineda@outlook.com>
2020-05-27 13:34:58 -0600

    Actualización de las dependencias

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
     },
 
     output : {
-        path : path.resolve(__dirname, '/public'),
+        path : path.resolve(__dirname, 'docs'),
         filename: 'js/main.js'
     },
 
